f9a67f8449bc5caf2f7e9c0fd3f36b20ed2ed240
Mawryshub <130665964+Mawryshub@users.noreply.github.com>
2023-07-20 21:10:33 -0300

    Visualizar publicaciones y cambio de aspecto
    Modifiqué algunas cosas relacionadas al aspecto de la página e hice que se mostraran las publicaciones en la pagina de publicaciones

--- DefiBlog/publicaciones/admin.py
+++ DefiBlog/publicaciones/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Publicaciones
+from .models import Publicaciones, Categoria
 # Register your models here.
 
 admin.site.register(Publicaciones)
+admin.site.register(Categoria)
