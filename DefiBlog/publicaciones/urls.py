d11c88175f527c3ade7f439bb74afb6ac26ac5e1
Mawryshub <130665964+Mawryshub@users.noreply.github.com>
2023-07-25 00:00:20 -0300

    Agregado comentarios en las publicaciones
    Agregadas funcionalidades para comentarios y un poco de autenticacion

--- DefiBlog/publicaciones/urls.py
+++ DefiBlog/publicaciones/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('crear-publicacion/', views.CrearPublicacion.as_view(), name='crear-publicacion'),
     path('editar-publicacion/<int:pk>', views.EditarPublicacion.as_view(), name='editar-publicacion'),
     path('eliminar-publicacion/<int:pk>', views.EliminarPublicacion.as_view(), name = 'eliminar-publicacion'),
+    path('detalle-post/<int:pk>', views.DetallePublicacion.as_view(), name='detalle-publicacion'),
+    path('borrar-comentario/<int:pk>', views.BorrarComentario.as_view(), name='borrar-comentario')
 ]
--- DefiBlog/publicaciones/views.py
+++ DefiBlog/publicaciones/views.py
@@ -1,8 +1,10 @@
+from typing import Any, Dict
 from django.shortcuts import render
-from publicaciones.models import Publicaciones
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from publicaciones.models import Publicaciones, Comentario
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.urls import reverse
-from .forms import CrearPublicacionForm
+from .forms import CrearPublicacionForm, ComentarioForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -15,7 +17,11 @@ class VerPublicaciones(ListView):
         consulta_anterior = super().get_queryset()
         return consulta_anterior.order_by('-fecha')
     
-class CrearPublicacion(CreateView):
+
+
+
+
+class CrearPublicacion(LoginRequiredMixin, CreateView):
     model = Publicaciones
     template_name = 'publicaciones/crear-publicacion.html'
     form_class = CrearPublicacionForm
@@ -23,7 +29,14 @@ class CrearPublicacion(CreateView):
     def get_success_url(self):
         return reverse('publicaciones:publicaciones')
     
-class EditarPublicacion(UpdateView):
+    def form_valid(self, form):
+            f = form.save(commit=False)
+            f.creador_id = self.request.user.id
+            return super().form_valid(f)
+
+
+
+class EditarPublicacion(LoginRequiredMixin, UpdateView):
     model = Publicaciones
     template_name = 'publicaciones/editar-publicacion.html'
     form_class = CrearPublicacionForm
@@ -31,9 +44,49 @@ class EditarPublicacion(UpdateView):
     def get_success_url(self):
         return reverse('publicaciones:publicaciones')
     
+
+
+
+    
 class EliminarPublicacion(DeleteView):
     model = Publicaciones
     template_name ='publicaciones/eliminar-publicacion.html'
     
     def get_success_url(self):
         return reverse('publicaciones:publicaciones')
+    
+
+
+
+class DetallePublicacion(DetailView):
+    template_name = 'publicaciones/detalle-publicacion.html'
+    model = Publicaciones
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comentario_form'] = ComentarioForm()
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        publicacion = self.get_object()
+        form = ComentarioForm(request.POST)
+
+        if form.is_valid():
+            comentario = form.save(commit=False)
+            comentario.creador_id = self.request.user.id
+            comentario.post = publicacion
+            comentario.save()
+            return super().get(request)
+        else:
+            return super().get(request)
+        
+
+
+
+class BorrarComentario(LoginRequiredMixin, DeleteView):
+    model = Comentario
+    template_name = 'publicaciones/borrar-comentario.html'
+
+    def get_success_url(self):
+        return reverse('publicaciones:detalle-publicacion', args = [self.object.post.id])
