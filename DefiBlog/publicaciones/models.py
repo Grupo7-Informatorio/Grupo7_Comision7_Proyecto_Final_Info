d1a56c7972585e35d93de75fce0f8d3a3a285fe6
Mawryshub <130665964+Mawryshub@users.noreply.github.com>
2023-07-27 17:28:47 -0300

    Modificado el modelo de publicaciones y comentarios
    Las fechas se guardaban en dias, por lo que publicaciones del mismo día no se acomodarían segun fecha, ahora tambien tiene horas, minutos y segundos

--- DefiBlog/publicaciones/models.py
+++ DefiBlog/publicaciones/models.py
@@ -11,8 +11,8 @@ class Categoria(models.Model):
         return self.nombre
 
 class Publicaciones(models.Model):
-    fecha = models.DateField(auto_now_add = True)
-    update = models.DateField(auto_now=True)
+    fecha = models.DateTimeField(auto_now_add = True)
+    update = models.DateTimeField(auto_now=True)
     titulo = models.CharField(max_length = 255)
     post = models.TextField()
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, related_name='posteos', null= True)
@@ -23,7 +23,7 @@ class Publicaciones(models.Model):
     
 class Comentario(models.Model):
     texto = models.TextField()
-    fecha = models.DateField(auto_now_add=True)
+    fecha = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey(Publicaciones, on_delete=models.CASCADE, related_name='comentarios')
     creador = models.ForeignKey(usuario, on_delete=models.CASCADE, related_name='comentarios')
 
