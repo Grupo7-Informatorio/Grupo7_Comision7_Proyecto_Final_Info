cdeade83e8763e5cd65bee53b4de4c9b3d8e9d7b
Mawryshub <130665964+Mawryshub@users.noreply.github.com>
2023-08-01 08:57:40 -0300

    Create prod.py

---
+++ DefiBlog/DefiBlog/prod.py
@@ -0,0 +1,15 @@
+from .settings import *
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
